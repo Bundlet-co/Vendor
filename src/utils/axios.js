4e3d34b598f399e6710d512eac716392a0630dac
dev_apus <folorunsopraise580@gmail.com>
2024-09-24 08:55:15 +0100

    Admin

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const dev_url = "http://localhost:3500/merchant"
+//export const dev_url = "http://localhost:3500/merchant"
+export const dev_url = "https://bundlet-server.onrender.com/merchant"
 
 export default axios.create( {
       baseURL: dev_url,
