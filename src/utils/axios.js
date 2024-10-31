f1d195d4153ecb1f2654cfa3c9a94c55dfe1a81b
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2024-10-31 01:49:26 +0100

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //export const dev_url = "http://localhost:3500/merchant"
-export const dev_url = "https://bundlet-server.onrender.com/merchant"
+export const dev_url = "https://bundlet-server.vercel.app/merchant"
 
 export default axios.create( {
       baseURL: dev_url,
